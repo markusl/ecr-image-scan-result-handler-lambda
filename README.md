6ced121b560e90d1ac57063a8ae1962ebcbf9030
Markus Lindqvist <markus.lindqvist@reaktor.com>
2019-11-17 16:23:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# ECR Image Scan Result Handler Lambda
+
+This repository contains an easy-to-deploy lambda handler to handle the ECR image scan results.
+
+To increase security finding handling in your project you might want to get notifications about issues in your images.
+
+[Learn more about ECR image scanning](https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html)
+
+What you should have in place:
+
+* ECR repositories
+* Image scanning enabled on them
+* A SNS topic that gets notified when there are image scan results available
+
+![AWS Architecture of ECR image scan result handler lambda](ECR_image_scan_handler.png)
+
+## What this lambda does
+
+When the lambda is triggered from the ECR image scan results it will go and fetch the image scan results to see if there are any reported problems.
+
+If scan finds any issues, the lambda will send an email to predefined address to notify the developers.
+
+The implementation consists of three parts:
+
+* `lambda-handler/` contains the actual lambda code
+* `lambda-runtime-layer/` contains a runtime layer for the lambda with the needed AWS SDK
+* `cdk/` contains deployment code for the functionality
+
+### Configuration
+
+Configure the SNS topic and sender address in the `cdk.json` file. You might have to verify the email in Amazon SES service.
+
+```json
+cdk.json
+{
+  "app": "node dist/index",
+  "context": {
+    "from_email": "from@example.com",
+    "target_email": "target@example.com",
+    "notification_topic": "arn:aws:sns:eu-west-1:992821666999:ExampleStack-ImageScanCompletedTopic08428532-RTLEXMO8349I"
+  }
+}
+```
+
+### Deployment
+
+In `cdk` directory run the following commands:
+
+```bash
+cdk cdk
+npm run build
+cdk deploy EcrImageScanResultHandler
+```
---
+++ cdk/index.ts
@@ -0,0 +1,94 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+import * as sns from '@aws-cdk/aws-sns';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as targets from '@aws-cdk/aws-events-targets';
+import * as sns_subs from '@aws-cdk/aws-sns-subscriptions';
+
+const basicLambdaPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole');
+const componentName = 'EcrImageScanResultHandler';
+
+export const createEcrImageScanResultHandlerStack = (
+    scope: cdk.Construct,
+    notificationTopicArn: string,
+    fromAddress: string,
+    toAddress: string,
+    props: cdk.StackProps) =>
+{
+    const lambdaStack = new cdk.Stack(scope, `${componentName}`, props);
+    const lambdaLayerCode = lambda.Code.fromAsset('../lambda-runtime-layer');
+    const lambdaCode = lambda.Code.fromAsset('../lambda-handler/dist');
+
+    const roleName = `${componentName}-role`;
+    const lambdaRole = new iam.Role(lambdaStack, roleName, {
+        roleName,
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        inlinePolicies: {
+            ecrPolicy: new iam.PolicyDocument({
+                statements: [new iam.PolicyStatement({
+                    actions: ['ecr:DescribeImageScanFindings'],
+                    resources: ['*'],
+                })],
+            }),
+            emailPolicy: new iam.PolicyDocument({
+                statements: [new iam.PolicyStatement({
+                    actions: ['ses:SendEmail'],
+                    resources: ['*'],
+                    conditions: {
+                    'StringEquals': {
+                        'ses:FromAddress': fromAddress,
+                    },
+                    }
+                })
+            ]}),
+        }
+    });
+    lambdaRole.addManagedPolicy(basicLambdaPolicy);
+
+    const lambdaLibLayer = new lambda.LayerVersion(lambdaStack, `${componentName}-AwsSdkLayer`, {
+        code: lambdaLayerCode,
+        compatibleRuntimes: [lambda.Runtime.NODEJS_10_X],
+        description: 'A layer to include AWS SDK for Lambda',
+    });
+
+    const ecrScanResultHandlerLambda = new lambda.Function(lambdaStack, `${componentName}`, {
+        functionName: `${componentName}`,
+        description: `Handler for ECR Image Scan results`,
+        runtime: lambda.Runtime.NODEJS_10_X,
+        handler: 'handler.handler',
+        role: lambdaRole,
+        code: lambdaCode,
+        layers: [lambdaLibLayer],
+        environment: {
+            FROM_ADDRESS: fromAddress,
+            TO_ADDRESS: toAddress,
+        }
+    });
+
+    const notificationTopic = sns.Topic.fromTopicArn(lambdaStack, `${componentName}-Topic`, notificationTopicArn);
+    notificationTopic.addSubscription(new sns_subs.LambdaSubscription(ecrScanResultHandlerLambda));
+};
+
+const app = new cdk.App();
+export const CdkEnvironment: cdk.Environment = {
+  // TODO Setup your variables
+  // region: 'eu-central-1',
+};
+
+// This is just some sample code for you to try this out:
+// 1. First deploy 'ExampleStack' (cdk deploy ExampleStack)
+// 2. Save the output ExampleStack.ImageScanCompletedTopicOutput and configure cdk.json
+// 3. Deploy stack EcrImageScanResultHandler
+const exampleStack = new cdk.Stack(app, 'ExampleStack');
+const exampleRepository = new ecr.Repository(exampleStack, `ExampleEcrRepository`);
+const exampleTopic = new sns.Topic(exampleStack, 'ImageScanCompletedTopic');
+exampleRepository.onImageScanCompleted(`ScanResults`, {
+    target: new targets.SnsTopic(exampleTopic),
+});
+new cdk.CfnOutput(exampleStack, `ImageScanCompletedTopicOutput`, { value: exampleTopic.topicArn });
+
+const fromAddress = app.node.tryGetContext('from_email');
+const toAddress = app.node.tryGetContext('target_email');
+const notificationTopicArn = app.node.tryGetContext('notification_topic');
+createEcrImageScanResultHandlerStack(app, notificationTopicArn, fromAddress, toAddress, { env: CdkEnvironment });
