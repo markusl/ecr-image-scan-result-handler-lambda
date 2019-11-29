5f85d5d3f4f5728be98e2d8d4bcecb8d2377e82f
Markus Lindqvist <markus.lindqvist@reaktor.com>
2019-11-29 10:08:04 +0200

    Minor code cleanups

--- lambda-handler/handler.ts
+++ lambda-handler/handler.ts
@@ -28,7 +28,7 @@ const sendEmail = async (subject: string, content: string) => {
 };
 
 exports.handler = async (event: Lambda.SNSEvent) => {
-    console.log(JSON.stringify(event));
+    console.log(JSON.stringify(event, undefined, 2)); 
 
     const imageScanCompletedEvent = JSON.parse(event.Records[0].Sns.Message);
 
@@ -40,42 +40,24 @@ exports.handler = async (event: Lambda.SNSEvent) => {
         repositoryName,
     };
 
-    const findings = await ecr.describeImageScanFindings(params).promise();
-    if (findings.$response.error || findings.imageScanFindings === undefined) {
-        throw new Error(JSON.stringify(findings.$response.error));
+    const findingsResult = await ecr.describeImageScanFindings(params).promise();
+    if (findingsResult.$response.error || !findingsResult.imageScanFindings) {
+        throw new Error(JSON.stringify(findingsResult.$response.error));
     }
 
-    if (!findings.imageScanFindings.findings) {
-        console.log(`No image scan findings for ${findings.repositoryName}`);
+    const findings = findingsResult.imageScanFindings.findings;
+    if (!findings) {
+        console.log(`No image scan findings for ${findingsResult.repositoryName}`);
         return;
     }
 
-    const numFindings = findings.imageScanFindings.findings.length;
-    const summary = `Got ${numFindings} security findings for ${repositoryName} at ${findings.imageScanFindings.imageScanCompletedAt}`;
-    if (numFindings) {
-        const details = findings.imageScanFindings.findings.map((finding) =>
-            `${finding.name} (${finding.severity}): ${finding.uri}`);
-        const combinedDetails = details.reduce((previous, current) => `${previous}\n\n${current}`);
+    const amountOfFindings = findings.length;
+    if (amountOfFindings > 0) {
+        const summary = `Got ${amountOfFindings} security findings for ${repositoryName} at ${findingsResult.imageScanFindings.imageScanCompletedAt}`;
+        const combinedDetails = findings.reduce((previous, current) =>
+            `${current.name} (${current.severity}): ${current.uri}\n\n${previous}`, '');
         console.log(summary);
         console.log(combinedDetails);
         await sendEmail(summary, combinedDetails);
     }
 };
-
-// Test event
-// {
-//     "version": "0",
-//     "id": "99c9ba1f-bba7-95c3-ca10-5d6dc8a992ee",
-//     "detail-type": "ECR Image Scan",
-//     "source": "aws.ecr",
-//     "account": "804416728801",
-//     "time": "2019-11-14T09:52:31Z",
-//     "region": "eu-central-1",
-//     "resources": ["arn:aws:ecr:eu-central-1:804416728801:repository/repository-name"],
-//     "detail": {
-//         "scan-status": "COMPLETE",
-//         "repository-name": "repository-name",
-//         "image-digest": "sha256:99c9ba1fbba795c3ca105d6dc8a992ee99c9ba1fbba795c3ca105d6dc8a992ee",
-//         "image-tags": ["99c9ba1fbba795c3ca105d6dc8a992ee", "latest"]
-//     }
-// };
